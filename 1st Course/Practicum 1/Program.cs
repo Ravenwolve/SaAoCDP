88a3bcfc6e58feeeaf9d9585f2e31a7ecb3bef05
Ravenwolve <91023927+Ravenwolve@users.noreply.github.com>
2022-09-07 22:37:50 +0400

    Add files via upload

---
+++ 1st Course/Practicum 1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace P1_10 {
+    internal class Program {
+        const short curyear = 2022;
+        static void Main(string[] args) {
+            Console.WriteLine("Как тебя зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Сколько тебе лет?");
+            short age = byte.Parse(Console.ReadLine());
+            Console.WriteLine(name + ", ты родился в {0}", curyear - age);
+        }
+    }
+}
---
+++ 1st Course/Practicum 2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace P1_10 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Проверка: Делится ли сумма цифр заданного трехзначного числа на другое заданное число.\nВведите трехзначное число: ");
+            short Number = short.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число-делитель: ");
+            byte A = byte.Parse(Console.ReadLine());
+            bool Answer = (Number % 10 + Number / 10 + Number / 100) % A == 0 ? true : false;
+            Console.WriteLine(Answer);
+        }
+    }
+}
