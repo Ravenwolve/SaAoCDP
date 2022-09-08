adaccbf14c6edecaf1f9ea1caecf24c2cba13a76
Ravenwolve <91023927+Ravenwolve@users.noreply.github.com>
2022-09-08 13:36:51 +0400

    Add files via upload

---
+++ 1st Course/Practicum 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace P1_10 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Все четные числа из диапазона от А до В, кратные трем.");
+            Console.Write("Введите число А: ");
+            int A = int.Parse(Console.ReadLine());
+            Console.Write("Введите число B: ");
+            int B = int.Parse(Console.ReadLine());
+            if (B < A)
+            {
+                int temp = A;
+                A = B;
+                B = A;
+            }
+            if (A % 6 != 0)
+            {
+                A += 6 - (A % 6);
+            }
+            for (int i = A; i <= B; i += 6)
+                Console.Write("{0} ", i);
+        }
+    }
+}
