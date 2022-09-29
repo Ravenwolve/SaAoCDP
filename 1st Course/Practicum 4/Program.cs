730479fb1d6094dcabfe2b5e8ed944d74d0a26ac
Ravenwolve <91023927+Ravenwolve@users.noreply.github.com>
2022-09-29 11:48:26 +0400

    Update Program.cs

--- 1st Course/Practicum 4/Program.cs
+++ 1st Course/Practicum 4/Program.cs
@@ -9,18 +9,26 @@ namespace P1_10
             Console.Write("Алгоритм: найти число, которое является суммой наибольшего и наименьшего простого множителя заданного числа.\nВведите число N: ");
             uint N = uint.Parse(Console.ReadLine()), d1 = 0, d2 = 0;
             Console.Write("Для числа {0} ", N);
-            for (uint d = 2; N != 1; d++)
-                if (N % d == 0)
-                {
-                    if (d1 == 0)
-                        d1 = d;
-                    else d2 = d;
-                    while (N % d == 0)
-                        N /= d;
-                }
-            if (d2 == 0)
-                Console.WriteLine(" найти сумму нельзя, так как оно простое.");
-            else Console.WriteLine("сумма его наибольшего и наименьшего простых множителей {0} и {1} равна {2}.\n", d1, d2, d1 + d2);
+            if (N == 1 || N == 0) {
+                Console.WriteLine("нет делителей.");
+            }
+            else
+            {
+                for (uint n = N, d = 2; n != 1; d++)
+                    if (n % d == 0)
+                    {
+                        if (d1 == 0)
+                            d1 = d;
+                        else d2 = d;
+                        while (n % d == 0)
+                            n /= d;
+                    }
+                if (d1 == N)
+                    Console.WriteLine("найти сумму нельзя, так как оно простое.");
+                else if (d2 == 0)
+                    Console.WriteLine("существует лишь один простой делитель - {0}.", d1);
+                else Console.WriteLine("сумма его наибольшего и наименьшего простых множителей {0} и {1} равна {2}.", d1, d2, d1 + d2);
+            }
         }
     }
 }
