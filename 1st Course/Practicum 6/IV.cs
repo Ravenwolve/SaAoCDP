67a42fe9df0dc1a26abdb80c97ec1d8eae66253b
Ravenwolve <91023927+Ravenwolve@users.noreply.github.com>
2022-10-13 13:40:21 +0400

    Update IV.cs

--- 1st Course/Practicum 6/IV.cs
+++ 1st Course/Practicum 6/IV.cs
@@ -1,48 +1,70 @@
-﻿using System;
+using System;
 
 namespace P1_10
 {
     internal class Program
     {
+        static void input_array(int[,] Arr)
+        {
+            for (ushort i = 0; i < Arr.GetLength(0); i++)
+                for (ushort j = 0; j < Arr.GetLength(0); j++)
+                {
+                    Console.Write("A[{0},{1}] = ", i, j);
+                    Arr[i, j] = int.Parse(Console.ReadLine());
+                }
+        }
+        static void output_array(int[,] Arr)
+        {
+            for (ushort i = 0; i < Arr.GetLength(0); i++, Console.WriteLine())
+                for (ushort j = 0; j < Arr.GetLength(0); j++)
+                    Console.Write("{0}\t", Arr[i, j]);
+        }
+        static void output_array(int[] Arr)
+        {
+            foreach (int item in Arr)
+                Console.Write("{0}\t", item);
+        }
+        static int[] each_first_positive_for(int[,] Arr)
+        {
+            ushort count = 0;
+            for (ushort i = 0; i < Arr.GetLength(0); i++)
+                for (ushort j = 0; j < Arr.GetLength(0); j++)
+                {
+                    if (Arr[j, i] > 0)
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            int[] Result = new int[count];
+            for (ushort i = 0, k = 0; i < Arr.GetLength(0); i++)
+                for (ushort j = 0; j < Arr.GetLength(0); j++)
+                {
+                    if (Arr[j, i] > 0)
+                    {
+                        Result[k] = Arr[j, i];
+                        k++;
+                        break;
+                    }
+                }
+            return Result;
+        }
         static void Main(string[] args)
         {
             Console.Write("Алгоритм: Для каждого столбца найти первый положительный элемент и записать данные в новый массив.\nВведите размерность квадратного массива: ");
             int N = int.Parse(Console.ReadLine());
-            int[,] A = new int[N,N];
-            for (ushort i = 0; i < N; i++)
-                for (ushort j = 0; j < N; j++)
-                {
-                    Console.Write("A[{0},{1}] = ", i, j);
-                    A[i, j] = int.Parse(Console.ReadLine());
-                }
-            /*
-    1    -2      -3       0
+            int[,] A = new int[N, N];
+            input_array(A);
+/*  1    -2      -3       0
    -5    -6      -7      -8
     9    -10     -11     12
    -13    14     -12     13
-            Вывод: 1 14 12
-*/
+            Вывод: 1 14 12 */
             Console.WriteLine("Ваш массив: ");
-            for (ushort i = 0; i < N; i++, Console.WriteLine())
-                for (ushort j = 0; j < N; j++)
-                    Console.Write("{0} ", A[i, j]);
-            ushort num = 0;
-            int[] Result = new int[N];
-            for (ushort i = 0; i < N; i++)
-                for (ushort j = 0; j < N; j++)
-                {
-                    if (A[j, i] > 0)
-                    {
-                        Result[num] = A[j, i];
-                        num++;
-                        break;
-                    }
-                }
-            int[] New_Result = new int[num];
-            Array.Copy(Result, New_Result, New_Result.Length);
+            output_array(A);
             Console.WriteLine("Получившийся массив:");
-            foreach (int item in New_Result)
-                Console.Write("{0} ", item);
+            int[] Result = each_first_positive_for(A);
+            output_array(Result);
         }
     }
 }
