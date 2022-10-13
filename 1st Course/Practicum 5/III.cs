59dadea69341941f33a8474d4edbef75f353071b
Ravenwolve <91023927+Ravenwolve@users.noreply.github.com>
2022-10-13 12:46:20 +0400

    Update III.cs

--- 1st Course/Practicum 5/III.cs
+++ 1st Course/Practicum 5/III.cs
@@ -1,39 +1,39 @@
-﻿using System;
+using System;
 
 namespace P1_10
 {
     internal class Program
     {
-        static double[,] get_minor(double[,] A, uint size, uint col)
+        static double[,] get_minor(double[,] A, uint col)
         {
-            double[,] minor = new double[size - 1, size - 1];
+            double[,] minor = new double[A.GetLength(0) - 1, A.GetLength(0) - 1];
             if (col == 0) //Если минор по первому элементу строки
-                for (ushort i = 1; i < size; i++)
-                    for (ushort j = 1; j < size; j++)
+                for (ushort i = 1; i < A.GetLength(0); i++)
+                    for (ushort j = 1; j < A.GetLength(0); j++)
                         minor[i - 1, j - 1] = A[i, j];
-            else for (ushort i = 1; i < size; i++)
+            else for (ushort i = 1; i < A.GetLength(0); i++)
                 {
                     for (ushort j = 0; j < col; j++)
                         minor[i - 1, j] = A[i, j];
-                    for (uint j = col + 1; j < size; j++)
+                    for (uint j = col + 1; j < A.GetLength(0); j++)
                         minor[i - 1, j - 1] = A[i, j];
                 }
             return minor;
         }
-        static double det(double[,] matrix, uint size)
+        static double det(double[,] matrix)
         {
-            if (size == 1)
+            if (matrix.GetLength(0) == 1)
                 return matrix[0, 0];
-            else if (size == 2)
+            else if (matrix.GetLength(0) == 2)
                 return matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
             else
             {
                 double sum = 0;
-                for (uint j = 0; j < size; j++)
+                for (uint j = 0; j < matrix.GetLength(0); j++)
                 {
                     if (j % 2 == 0)
-                        sum += matrix[0, j] * det(get_minor(matrix, size, j), size - 1);
-                    else sum -= matrix[0, j] * det(get_minor(matrix, size, j), size - 1);
+                        sum += matrix[0, j] * det(get_minor(matrix, j));
+                    else sum -= matrix[0, j] * det(get_minor(matrix, j));
                 }
                 return sum;
             }
@@ -46,10 +46,10 @@ namespace P1_10
             for (ushort i = 0; i < N; i++)
                 for (ushort j = 0; j < N; j++)
                 {
-                    Console.Write("Введите элемент A[{0},{1}]: ", i, j);
+                    Console.Write("Введите элемент A[{0},{1}]: ", i + 1, j + 1);
                     Arr[i, j] = double.Parse(Console.ReadLine());
                 }
-            Console.WriteLine("Детерминант заданной матрицы равен {0}", det(Arr, N));
+            Console.WriteLine("Детерминант заданной матрицы равен {0}", det(Arr));
         }
     }
 }
