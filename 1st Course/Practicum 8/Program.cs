7b7401c2eb616aa4afdac7af6d37fd1ba1b9184c
Ravenwolve <91023927+Ravenwolve@users.noreply.github.com>
2022-10-20 12:22:38 +0400

    Add files via upload

---
+++ 1st Course/Practicum 8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P1_10
+{
+    internal class Program
+    {
+        static int FindAmountOfUppercaseWords(string String)
+        {
+            List<string> Words = new List<string>();
+            for (int i = 0, j = 0; j < String.Length; j++)
+                if (!char.IsLetter(String[j]))
+                {
+                    if (i != j)
+                        Words.Add(String.Substring(i, j - i));
+                    i = j + 1;
+                }
+            int Counter = Words.Count;
+            foreach (string ItemStr in Words)
+                foreach (char ItemCh in ItemStr)
+                    if (char.IsLower(ItemCh))
+                    {
+                        Counter--;
+                        break;
+                    }
+            return Counter;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Алгоритм: Найти количество слов, состоящих только из прописных букв.\nВведите строку: ");
+            string MyStr = Console.ReadLine();
+            //    НА?!ДВОРЕ ТРАВа, на ТРАВЕ ДРоВА, НЕ?,!Руби ДРОВА на ТРАВЕ двора!
+            //Вывод: 6
+            Console.WriteLine(FindAmountOfUppercaseWords(MyStr));
+        }
+    }
+}
