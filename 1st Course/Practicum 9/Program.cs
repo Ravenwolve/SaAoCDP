e31e2986628a271c0420704a4c1343f7b9777dc2
Ravenwolve <91023927+Ravenwolve@users.noreply.github.com>
2022-10-26 22:20:41 +0400

    Add files via upload

---
+++ 1st Course/Practicum 9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace P1_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Алгоритм: Дан файл f, компонентами которого являются символы. Переписать в файл g все знаки препинания файла f, а в файл h – все остальные символы файла f.");
+            using (StreamReader FileInF = new StreamReader("C:/Users/Harvey/source/repos/P1-10/P1-10/f.txt", Encoding.GetEncoding(1251))) //C:/Users/sharovkv/source/repos/P1-10/P1-10/f.txt
+            {
+                using (StreamWriter FileOutG = new StreamWriter("C:/Users/Harvey/source/repos/P1-10/P1-10/g.txt", true), FileOutH = new StreamWriter("C:/Users/Harvey/source/repos/P1-10/P1-10/h.txt", true))
+                {
+                    string Line = FileInF.ReadToEnd();
+                    for (int i = 0; i < Line.Length; i++)
+                    {
+                        if (!char.IsPunctuation(Line[i]))
+                            FileOutH.Write(Line[i]); //или лучше построчно писать?
+                        else FileOutG.Write(Line[i]);
+                    }
+                    Console.WriteLine("Работа программы успешно завершена.");
+                }
+            }
+        }
+    }
+}
