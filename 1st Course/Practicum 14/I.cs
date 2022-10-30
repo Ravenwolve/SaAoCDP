49e411660a2fa3c5e1d01e07ebc4a34d143e8799
Ravenwolve <91023927+Ravenwolve@users.noreply.github.com>
2022-10-30 13:19:11 +0400

    Add files via upload

---
+++ 1st Course/Practicum 14/I.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+namespace P1_10
+{
+    struct SPoint
+    {
+        public double x, y, z;
+        public SPoint(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        public static bool IsInOneLot(SPoint FirstPoint, SPoint SecondPoint, uint Radius)
+        {
+            return ((FirstPoint.x - SecondPoint.x) * (FirstPoint.x - SecondPoint.x)
+                + (FirstPoint.y - SecondPoint.y) * (FirstPoint.y - SecondPoint.y) +
+                (FirstPoint.z - SecondPoint.z) * (FirstPoint.z - SecondPoint.z)) <= Radius * Radius;
+
+        }
+    }
+    internal class Program
+    {
+        static double Mod(double Number)
+        {
+            if (Number >= 0)
+                return Number;
+            else return -Number;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Алгоритм: Найти такую точку, что шар радиуса R с центром в этой точке содержит максимальное число точек заданного множества.");
+            using (StreamReader FileIn = new StreamReader("C:/Users/Harvey/source/repos/P1-10/P1-10/input.txt", Encoding.GetEncoding(1251))) //C:/Users/sharovkv/source/repos/P1-10/P1-10/f.txt
+            {
+                string Line;
+                string[] Nums;
+                char[] Separators = { ' ', ',', ';' };
+                SPoint Temp = new SPoint();
+                List<SPoint> Points = new List<SPoint>();
+                while ((Line = FileIn.ReadLine()) != null)
+                {
+                    Nums = Line.Split(Separators, StringSplitOptions.RemoveEmptyEntries);
+                    Temp.x = double.Parse(Nums[0]);
+                    Temp.y = double.Parse(Nums[1]);
+                    Temp.z = double.Parse(Nums[2]);
+                    Points.Add(Temp);
+                }
+                Console.Write("Точки успешно считаны. Введите значение радиуса R: ");
+                uint R = uint.Parse(Console.ReadLine());
+                short Counter = -1, MaxCounter = 0;
+                foreach (SPoint FirstItem in Points)
+                {
+                    foreach (SPoint SecondItem in Points)
+                        if (SPoint.IsInOneLot(FirstItem, SecondItem, R))
+                            Counter++;
+                    if (Counter > MaxCounter)
+                    {
+                        MaxCounter = Counter;
+                        Temp = FirstItem;
+                    }
+                    Counter = -1;
+                }
+                if (MaxCounter == 0)
+                    Console.WriteLine("Таких точек не найдено.");
+                else Console.WriteLine("Найдена точка ({0};{1};{2}). Количество точек, находящихся в радиусе этой точки: {3}", Temp.x, Temp.y, Temp.z, MaxCounter);
+            }
+        }
+    }
+}
+
---
+++ 1st Course/Practicum 14/II.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+
+namespace P1_10
+{
+    struct Student : IComparable<Student>
+    {
+        public string Surname, Name, Lastname;
+        public byte Course;
+        public ushort Group;
+        public double Result;
+        public Student(string Surname, string Name, string Lastname, byte Course, ushort Group, double Result)
+        {
+            this.Surname = Surname;
+            this.Name = Name;
+            this.Lastname = Lastname;
+            this.Course = Course;
+            this.Group = Group;
+            this.Result = Result;
+        }
+        public int CompareTo(Student Object) { return this.Result.CompareTo(Object.Result); }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Алгоритм: Найти такую точку, что шар радиуса R с центром в этой точке содержит максимальное число точек заданного множества.");
+            using (StreamReader FileIn = new StreamReader("C:/Users/Harvey/source/repos/P1-10/P1-10/input.txt", Encoding.GetEncoding(1251))) //C:/Users/sharovkv/source/repos/P1-10/P1-10/f.txt
+            {
+                string Line;
+                string[] Nums;
+                char[] Separators = { ' ', ';', '(', ')', '-' };
+                Student Temp = new Student();
+                List<Student> Students = new List<Student>();
+                while ((Line = FileIn.ReadLine()) != null)
+                {
+                    Nums = Line.Split(Separators, StringSplitOptions.RemoveEmptyEntries);
+                    Temp.Surname = String.Copy(Nums[0]);
+                    Temp.Name = String.Copy(Nums[1]);
+                    Temp.Lastname = String.Copy(Nums[2]);
+                    Temp.Course = byte.Parse(Nums[3]);
+                    Temp.Group = ushort.Parse(Nums[4]);
+                    Temp.Result = double.Parse(Nums[5]);
+                    Students.Add(Temp);
+                }
+                Nums = null;
+                Console.WriteLine("Данные студентов успешно считаны. Процесс сортировки...");
+                Students.Sort();
+                Console.WriteLine("Сортировка данных завершена. Процесс вывода данных в output.txt...");
+                using (StreamWriter FileOut = new StreamWriter("C:/Users/Harvey/source/repos/P1-10/P1-10/output.txt", true))
+                {
+                    Temp = Students[0];
+                    byte Counter = 1;
+                    foreach (Student Item in Students)
+                    {
+                        if (Item.CompareTo(Temp) == 0) ;
+                        else if (Counter < 3)
+                        {
+                            Counter++;
+                            Temp = Item;
+                        }
+                        else break;
+                        FileOut.WriteLine("{0}, {1} {2} {3} ({4}) - {5};", Item.Surname, Item.Name, Item.Lastname, Item.Course, Item.Group, Item.Result);
+                    }
+                    Console.WriteLine("Данные о студентах успешно выведены в файл output.txt.");
+                }
+            }
+        }
+    }
+}
+
+
