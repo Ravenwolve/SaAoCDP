70fb48445c38692729814300d82b3639e0d32311
Ravenwolve <91023927+Ravenwolve@users.noreply.github.com>
2022-11-17 00:29:32 +0400

    Add files via upload

---
+++ 1st Course/Practicum 16/I.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace P1_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader FileIn = new StreamReader("C:/Users/Harvey/source/repos/P1-10/P1-10/input.txt", Encoding.GetEncoding(1251)))
+            {
+                char[] Separators = { ' ', ',', '.', ';' };
+                string[] File = FileIn.ReadToEnd().Split(Separators, StringSplitOptions.RemoveEmptyEntries);
+                int[] Numbers = new int[File.Length];
+                for (int i = 0; i < File.Length; i++)
+                    Numbers[i] = int.Parse(File[i]);
+                //var Numbers = Convert.ToInt32(File);
+                var Result = Numbers.Where(n => n > 9 && n < 100 || n > -100 && n < -9).Select(n => n + 5);
+                foreach (int Item in Result)
+                    Console.Write("{0} ", Item);
+                //521 52 754 23 -259 -1234 5256 21 23 99 021 123 45 2
+            }
+        }
+    }
+}
---
+++ 1st Course/Practicum 16/II.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace P1_10
+{
+    internal class Program
+    {
+        struct Employee
+        {
+            public string Surname, Name, Lastname;
+            public ushort BeginningDate;
+            public string Function;
+            public uint Wage;
+            public byte ExperienceYears;
+            public Employee(string Surname, string Name, string Lastname, ushort BeginningDate, string Function, uint Wage, byte ExperienceYears)
+            {
+                this.Surname = Surname;
+                this.Name = Name;
+                this.Lastname = Lastname;
+                this.BeginningDate = BeginningDate;
+                this.Function = Function;
+                this.Wage = Wage;
+                this.ExperienceYears = ExperienceYears;
+            }
+        }
+        static void Main(string[] args)
+        {
+            using (StreamReader FileIn = new StreamReader("C:/Users/Harvey/source/repos/P1-10/P1-10/input.txt", Encoding.GetEncoding(1251)))
+            {
+                using (StreamWriter FileOut = new StreamWriter("C:/Users/Harvey/source/repos/P1-10/P1-10/output.txt", true))
+                {
+                    Console.Write("Введите границу зарплаты для вывода информации сотрудников с зарплатой ниже на экран (с сортировкой по стажу): ");
+                    uint Wage = uint.Parse(Console.ReadLine());
+                    char[] Separators = { ' ', ',', '.', ';' };
+                    string[] Data;
+                    List<Employee> Employees = new List<Employee>();
+                    Employee Temp;
+                    string Line;
+                    while ((Line = FileIn.ReadLine()) != null)
+                    {
+                        Data = Line.Split(Separators, StringSplitOptions.RemoveEmptyEntries);
+                        Temp.Surname = Data[0];
+                        Temp.Name = Data[1];
+                        Temp.Lastname = Data[2];
+                        Temp.BeginningDate = ushort.Parse(Data[3]);
+                        Temp.Function = Data[4];
+                        Temp.Wage = uint.Parse(Data[5]);
+                        Temp.ExperienceYears = byte.Parse(Data[6]);
+                        Employees.Add(Temp);
+                    }
+                    var Result = Employees.Where(Emp => Emp.Wage < Wage).OrderByDescending(Emp => Emp.ExperienceYears);
+                    foreach (Employee Item in Result)
+                        FileOut.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}", Item.Surname, Item.Name, Item.Lastname, Item.BeginningDate, Item.Function, Item.Wage, Item.ExperienceYears);
+                    /*
+                     Sharov Kirill Vladimirovich 2021 Programmer 75000 5
+Sergeev Alexei Andreevich 2017 Designer 80000 9
+Androsov Pyotr Vasilievich 2020 Programmer 100000 8
+Leibniz Anton Borisovich 2019 System-Admin 45000 3
+Fyodorov Miron Yanovich 2015 Designer 89500 12
+                     */
+                }
+            }
+        }
+    }
+}
