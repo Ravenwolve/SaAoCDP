a05530b757ce97fb03d26f971ef203916beaec18
Ravenwolve <91023927+Ravenwolve@users.noreply.github.com>
2022-11-23 23:36:09 +0400

    Add files via upload

---
+++ 1st Course/Practicum 17/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace P1_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringDoing example = new StringDoing("На ДвОрЕ трава, на траве дрова - не руби дрова на траве ДВОРА!");
+            Console.WriteLine(example);
+            Console.WriteLine(StringDoing.CountSpaces(example));
+            example.ToLower();
+            Console.WriteLine(example);
+            example.RemovePunctuation();
+            Console.WriteLine(example);
+            example.Line = new StringBuilder("f");
+        }
+    }
+}
