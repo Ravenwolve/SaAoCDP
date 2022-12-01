e2e54a770c07f60cd13a3b0dbc687d4a67e15062
Ravenwolve <91023927+Ravenwolve@users.noreply.github.com>
2022-12-01 13:29:57 +0400

    Update StringDoing.cs

--- 1st Course/Practicum 17/StringDoing.cs
+++ 1st Course/Practicum 17/StringDoing.cs
@@ -10,7 +10,27 @@ namespace P1_10
         public StringBuilder Line { set { line = new StringBuilder(Console.ReadLine()); } get { return line; } }
         public StringDoing() { line = new StringBuilder(""); }
         public StringDoing(string StringData) { line = new StringBuilder(StringData); }
-        public int this[int i] { get { return line[i]; } }
+        public int this[int i] { 
+            get {
+                if (i < 0 || i > Length)
+                {
+                    Console.WriteLine("Индекс {0} выходит за границы строки", i);
+                    return 0;
+                }
+                else return Line[i];
+            }
+            set
+            {
+                if (i < 0 || i > Length)
+                {
+                    Console.WriteLine("Индекс {0} выходит за границы строки", i);
+                }
+                else
+                {
+                    line[i] = (char)value;
+                }
+            }
+        }
         public static int CountSpaces(StringDoing line) //Метод подсчета количества пробелов
         {
             int count = 0;
@@ -41,11 +61,12 @@ namespace P1_10
             return null;
         }
         public static StringDoing operator + (StringDoing line) { return line.ToUpper(); }
-        public static StringDoing operator -(StringDoing line) { return line.ToLower(); }
+        public static StringDoing operator - (StringDoing line) { return line.ToLower(); }
         public static bool operator true(StringDoing line) { return line.Length != 0; }
         public static bool operator false(StringDoing line) { return line.Length == 0; }
         public static bool operator &(StringDoing line_1, StringDoing line_2) { return line_1 == line_2; }
+        public static implicit operator StringBuilder(StringDoing line) { return new StringBuilder(line.ToString()); }
+        public static implicit operator StringDoing(StringBuilder line) { return new StringDoing(line.ToString()); }
         public override string ToString() { return line.ToString(); }
-        public static implicit operator StringBuilder(StringDoing line) { return line.Line; }
     }
 }
