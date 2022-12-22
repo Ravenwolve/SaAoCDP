b1733aacc8e10e452b7c1a48d5bbe0d37b0899e7
Ravenwolve <91023927+Ravenwolve@users.noreply.github.com>
2022-12-22 12:54:54 +0400

    Update Circle.cs

--- 1st Course/Practicum 18/Circle.cs
+++ 1st Course/Practicum 18/Circle.cs
@@ -1,14 +1,33 @@
-﻿using System;
+using System;
 
 namespace P1_10
 {
     internal class Circle : Figure
     {
         protected double r;
-        public Circle() { r = 0; }
-        public Circle(double radius) { r = radius; }
-        public override double Square() { return Math.PI * r * r; }
-        public override double Perimeter() { return 2 * Math.PI * r; }
-        public override string ToString() { return r.ToString(); }
+        public Circle()
+        {
+            r = 0;
+        }
+        public Circle(double radius)
+        {
+            r = radius;
+        }
+        public Circle(Circle Object)
+        {
+            r = Object.r;
+        }
+        public override double Square()
+        { 
+            return Math.PI * r * r; 
+        }
+        public override double Perimeter()
+        {
+            return 2 * Math.PI * r;
+        }
+        public override string ToString()
+        {
+            return r.ToString();
+        }
     }
 }
