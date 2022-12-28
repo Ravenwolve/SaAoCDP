a66dcd3786d48a37177097c95a29f32c7d793146
Ravenwolve <91023927+Ravenwolve@users.noreply.github.com>
2022-12-29 00:13:00 +0400

    Add files via upload

---
+++ 1st Course/Practicum 13/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace P1_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader fileIn = new StreamReader("C:/Users/Harvey/source/repos/P1-10/P1-10/input.txt"))
+            {
+                string text = fileIn.ReadToEnd();
+                Console.WriteLine("Исходный текст:\n" + text);
+                //Первый год (лучше через простую замену в строке)
+                text = new Regex(@"C([ie])").Replace(text, "S${1}");//Ci & Ce => Si & Se
+                text = new Regex(@"c([ie])").Replace(text, "s${1}");//ci & ce => si & se
+                text = new Regex(@"ck|c").Replace(text, "k");       //ck & c => k
+                text = text.Replace('C', 'K');
+                //Второй год
+                text = new Regex(@"e{2,}").Replace(text, "i");
+                text = new Regex(@"o{2,}").Replace(text, "u");
+                text = new Regex(@"([a-z|A-Z])\1+").Replace(text, "${1}");
+                //Третий год
+                text = new Regex(@"(\w+)e(\W)").Replace(text, "${1}${2}");
+                //Четвертый год
+                text = new Regex(@"(\b|[-,:.!?])([Aa]|[Aa]n|[Tt]h)(\b|[-,:.!?])").Replace(text, "${1}${3}");
+                //Форматирование по пробелам
+                text = new Regex(@"( )+").Replace(text, "${1}");
+                Console.WriteLine("\nИскомый текст:\n" + text);
+                using (StreamWriter fileOut = new StreamWriter("C:/Users/Harvey/source/repos/P1-10/P1-10/output.txt"))
+                {
+                    fileOut.Write(text);
+                }
+            }
+        }
+    }
+}
